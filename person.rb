50462504f1cd06a4687def4b3c63fcea503d691f
Michael <madetunji1376@gmail.com>
2022-10-06 22:51:16 +0100

    reviewer suggestions worked on

--- person.rb
+++ person.rb
@@ -1,12 +1,13 @@
 require './nameable'
 
 class Person < Nameable
-  def initialize(age, name = 'Unknown', _parent_permission: true)
+  def initialize(age, rentals, name = 'Unknown', _parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @_parent_permission = parent_permission
+    @rentals = rentals
     @visits = []
   end
   attr_reader :id, :rentals
